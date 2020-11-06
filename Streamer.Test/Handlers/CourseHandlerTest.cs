3a5d1f438b2db84cd83e9a3e66af17fcb57eb291
DevLucasOliveira <lucasoliveira.si@outlook.com>
2020-11-06 11:25:35 -0300

    Testes unitários no handler

---
+++ Streamer.Test/Handlers/CourseHandlerTest.cs
@@ -0,0 +1,135 @@
+﻿using Moq;
+using Moq.AutoMock;
+using Streamer.Domain.Commands;
+using Streamer.Domain.Entities;
+using Streamer.Domain.Handlers;
+using Streamer.Domain.Repositories;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Streamer.Test.Handlers
+{
+    public class CourseHandlerTest
+    {
+        private AutoMocker _mocker;
+
+        public CourseHandlerTest()
+        {
+            _mocker = new AutoMocker();
+            
+        }
+
+        [Fact]
+        public async Task CreateCourse_WhenCommandIsValid_ReturnShouldBeOk()
+        {
+            // Arrange
+            var command = CreateCommand();
+            var course = _mocker.CreateInstance<CourseHandler>();
+
+            // Act
+            command.Validate();
+
+            var requestResult = await course.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.True(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task CreateCourse_WhenCommandIsInValid_ReturnShouldBeError()
+        {
+            // Arrange
+            var command = CreateCommandInvalid();
+            var course = _mocker.CreateInstance<CourseHandler>();
+            // Act
+            command.Validate();
+            var requestResult = await course.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.False(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task UpdateCourse_WhenCommandIsValid_ReturnShouldBeOk()
+        {
+            // Arrange
+            var command = UpdateCommand();
+            var course = _mocker.CreateInstance<CourseHandler>();
+            _mocker.GetMock<ICourseRepository>().Setup(x => x.GetById(It.IsAny<Guid>()))
+                .Returns(GetCourse());
+
+            // Act
+            command.Validate();
+
+            var requestResult = await course.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.True(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task UpdateCourse_WhenCommandIsInValid_ReturnShouldBeError()
+        {
+            // Arrange
+            var command = UpdateCommandInvalid();
+            var course = _mocker.CreateInstance<CourseHandler>();
+            // Act
+            command.Validate();
+            var requestResult = await course.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.False(requestResult.Success);
+        }
+
+        public Course GetCourse()
+        {
+            return new Course("Teste");
+        }
+
+        public CreateCourseCommand CreateCommand()
+        {
+            return new CreateCourseCommand()
+            {
+                Name = "Teste"
+            };
+        }
+
+        public CreateCourseCommand CreateCommandInvalid()
+        {
+            return new CreateCourseCommand()
+            {
+                Name = ""
+            };
+        }
+
+        public UpdateCourseCommand UpdateCommand()
+        {
+            return new UpdateCourseCommand()
+            {
+                Name = "Teste"
+            };
+        }
+
+        public UpdateCourseCommand UpdateCommandInvalid()
+        {
+            return new UpdateCourseCommand()
+            {
+                Name = ""
+            };
+        }
+    }
+}
---
+++ Streamer.Test/Handlers/ProjectHandlerTest.cs
@@ -0,0 +1,205 @@
+﻿using Moq;
+using Moq.AutoMock;
+using Streamer.Domain.Commands;
+using Streamer.Domain.Entities;
+using Streamer.Domain.Enums;
+using Streamer.Domain.Handlers;
+using Streamer.Domain.Repositories;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Streamer.Test.Handlers
+{
+    public class ProjectHandlerTest
+    {
+        private AutoMocker _mocker;
+
+        public ProjectHandlerTest()
+        {
+            _mocker = new AutoMocker();
+
+        }
+
+        [Fact]
+        public async Task CreateProject_WhenCommandIsValid_ReturnShouldBeOk()
+        {
+            // Arrange
+            var command = CreateCommand();
+            var project = _mocker.CreateInstance<ProjectHandler>();
+            _mocker.GetMock<ICourseRepository>().Setup(x => x.GetById(It.IsAny<Guid>()))
+                .Returns(GetCourse());
+
+            // Act
+            command.Validate();
+
+            var requestResult = await project.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.True(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task CreateProject_WhenCommandIsInValid_ReturnShouldBeError()
+        {
+            // Arrange
+            var command = CreateCommandInvalid();
+            var project = _mocker.CreateInstance<ProjectHandler>();
+            // Act
+            command.Validate();
+            var requestResult = await project.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.False(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task CreateProject_WhenCourseNotExists_ReturnShouldBeError()
+        {
+            // Arrange
+            var command = CreateCommand();
+            var project = _mocker.CreateInstance<ProjectHandler>();
+            _mocker.GetMock<ICourseRepository>().Setup(x => x.GetById(It.IsAny<Guid>()));
+
+            // Act
+            command.Validate();
+
+            var requestResult = await project.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.False(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task UpdateProject_WhenCommandIsValid_ReturnShouldBeOk()
+        {
+            // Arrange
+            var command = UpdateCommand();
+            var project = _mocker.CreateInstance<ProjectHandler>();
+            _mocker.GetMock<IProjectRepository>().Setup(x => x.GetById(It.IsAny<Guid>()))
+                .Returns(GetProject());
+
+            // Act
+            command.Validate();
+
+            var requestResult = await project.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.True(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task UpdateProject_WhenCommandIsInValid_ReturnShouldBeError()
+        {
+            // Arrange
+            var command = UpdateCommandInvalid();
+            var project = _mocker.CreateInstance<ProjectHandler>();
+            // Act
+            command.Validate();
+            var requestResult = await project.Handle(command, CancellationToken.None);
+
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.False(requestResult.Success);
+        }
+
+        [Fact]
+        public async Task UpdateProject_WhenProjectNotExists_ReturnShouldBeError()
+        {
+            // Arrange
+            var command = UpdateCommandInvalid();
+            var project = _mocker.CreateInstance<ProjectHandler>();
+            _mocker.GetMock<IProjectRepository>().Setup(x => x.GetById(It.IsAny<Guid>()));
+
+            // Act
+            command.Validate();
+            var requestResult = await project.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(requestResult);
+            Assert.NotEmpty(requestResult.Message);
+            Assert.False(requestResult.Success);
+        }
+
+        public Course GetCourse()
+        {
+            return new Course("Teste");
+        }
+
+        public Project GetProject()
+        {
+            return new Project("Teste", "Teste", "Teste", "Teste", "Teste", ProjectStatus.Desenvolvimento);
+        }
+
+        public CreateProjectCommand CreateCommand()
+        {
+            return new CreateProjectCommand()
+            {
+                Name = "Teste",
+                Image = "Teste",
+                Why = "Teste",
+                What = "Teste",
+                WhatWillWeDo = "Teste",
+                ProjectStatus = ProjectStatus.Desenvolvimento,
+                CourseId = Guid.NewGuid().ToString()
+            };
+        }
+
+        public CreateProjectCommand CreateCommandInvalid()
+        {
+            return new CreateProjectCommand()
+            {
+                Name = "",
+                Image = "Teste",
+                Why = "Teste",
+                What = "Teste",
+                WhatWillWeDo = "Teste",
+                ProjectStatus = ProjectStatus.Desenvolvimento,
+                CourseId = ""
+            };
+        }
+
+        public UpdateProjectCommand UpdateCommand()
+        {
+            return new UpdateProjectCommand()
+            {
+                Name = "Teste",
+                Image = "Teste",
+                Why = "Teste",
+                What = "Teste",
+                WhatWillWeDo = "Teste",
+                ProjectStatus = ProjectStatus.Desenvolvimento,
+                CourseId = Guid.NewGuid().ToString()
+            };
+        }
+
+        public UpdateProjectCommand UpdateCommandInvalid()
+        {
+            return new UpdateProjectCommand()
+            {
+                Name = "",
+                Image = "Teste",
+                Why = "Teste",
+                What = "Teste",
+                WhatWillWeDo = "Teste",
+                ProjectStatus = ProjectStatus.Desenvolvimento,
+                CourseId = ""
+            };
+        }
+    }
+}
